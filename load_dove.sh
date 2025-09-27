6965c0ad915f13767acd7d53b566a4f9f5fb6db7
Jon Warbrick <jon.warbrick@googlemail.com>
2025-09-27 12:37:52 +0100

    Add missing database filename

--- load_dove.sh
+++ load_dove.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sqlite3 "drop table if exists dove_towers" ".mode csv" ".import dove.csv dove_towers"
+sqlite3 eda/db.sqlite3 "drop table if exists dove_towers" ".mode csv" ".import dove.csv dove_towers"
