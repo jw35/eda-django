78be3f9f27ec138c0cda2195a1d5ca383b3652ad
Jon Warbrick <jon.warbrick@googlemail.com>
2025-09-29 14:30:03 +0100

    Add email config (via Google) for production

--- eda/eda/production_settings.py
+++ eda/eda/production_settings.py
@@ -10,3 +10,11 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "jon.warbrick@googlemail.com"
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
