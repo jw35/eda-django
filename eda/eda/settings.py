6adefbdfbb13c03dc4c2e2d6cbf3b185afac563f
Jon Warbrick <jon.warbrick@googlemail.com>
2025-12-10 14:17:51 +0000

    Drop cache configuration - no longer needed

--- eda/eda/settings.py
+++ eda/eda/settings.py
@@ -93,12 +93,6 @@ DATABASES = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
